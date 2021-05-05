094003345b66adf7bc0e63a7990d0573a7f1b40f
pgomezboza <pgomezboza@gmail.com>
2021-05-04 23:42:05 -0500

    update

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for n in row:
+            if n is not row[len(row) - 1]:
+                print("{:d}".format(n), end=" ")
+            else:
+                print("{:d}".format(n), end="")
+        print()
