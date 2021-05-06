9cb95680c9a196af803d5adfe9671609e672156c
pgomezboza <pgomezboza@gmail.com>
2021-05-06 18:47:39 -0500

    add task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-#
-# search_replace - function that replaces all occurrences
-# of an element by another in a new list.
-# @my_list: initial list
-# @search: element to replace
-# @replace: new element
-# Return: new list
 def search_replace(my_list, search, replace):
     new_list = []
     for n in my_list:
         if(n == search):
             new_list.append(replace)
         else:
-            new_list.append(n)
+            new_list.append
     return (new_list)
