de8f88e5ca10190cec1728f2396a07fc884a1b1e
pgomezboza <pgomezboza@gmail.com>
2021-06-01 21:51:19 -0500

    update task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
 Module: 1-my_list
 """
 
@@ -8,6 +8,11 @@ class MyList(list):
     """
     Class MyList inherits from list.
     """
+    def __init__(self):
+        """
+        initializes the object
+        """
+        super().__init__()
 
     def print_sorted(self):
         """
