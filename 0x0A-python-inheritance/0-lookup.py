db6781e76b39686feddd38f1cc3c77fd2ad5ca6d
pgomezboza <pgomezboza@gmail.com>
2021-06-01 21:16:17 -0500

    add task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module: 0-lookup.py
+"""
+
+
+def lookup(obj):
+    """
+    lookup: returns that list of attributes and methods.
+    Args: object to search.
+    """
+    return (dir(obj))
