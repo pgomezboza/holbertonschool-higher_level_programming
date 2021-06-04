83e0630d1f795ffb45b1ecd154c285cfe0bc3ea8
pgomezboza <pgomezboza@gmail.com>
2021-06-03 19:33:54 -0500

    add task 5

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-Module: 4-inherits_from 
+Module: 4-inherits_from
 """
 
 
 def inherits_from(obj, a_class):
     """
-    Evaluate if the object is an instance of a class 
+    Evaluate if the object is an instance of a class
     that inherited from the specified class.
     """
     if isinstance(obj, a_class) and type(obj) != a_class:
