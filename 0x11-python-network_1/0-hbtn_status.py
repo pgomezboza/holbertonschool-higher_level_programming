72ae44bec38a4fe6f6efa4c5c0d29ba83fa93cca
pgomezboza <pgomezboza@gmail.com>
2021-08-20 21:18:45 -0500

    fix errors

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-# Python script that fetches https://intranet.hbtn.io/status
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
 
 import urllib.request
 
 if __name__ == "__main__":
-
     url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        website = response.read()
-        print("Body response:")
+    with urllib.request.urlopen(url) as resp:
+        website = resp.read()
+        print('Body response:')
         print("\t- type: {}".format(type(website)))
         print("\t- content: {}".format(website))
         print("\t- utf8 content: {}".format(website.decode('utf-8')))
