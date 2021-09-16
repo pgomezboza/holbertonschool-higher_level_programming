004e4e212bfcd8cbfe7fa61ab358cc804283e721
pgomezboza <pgomezboza@gmail.com>
2021-09-15 23:02:16 -0500

    add task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// script that gets the contents of a webpage and stores it in a file.
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filename = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(filename, body, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
